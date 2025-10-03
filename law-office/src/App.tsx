fa7559a427a4c72fdc353366299ae39065279ff7
ItsJuniorDias <reabilitado97@gmail.com>
2025-10-03 07:47:49 -0300

    chore: create project and config tailwind

---
+++ law-office/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen bg-gray-100 font-roboto">
+        <h1 className="text-4xl font-bold text-blue-600">
+          React + Vite + Tailwind + Roboto
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
